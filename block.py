e0cf7ce25ef3dcd83671ee191b25280ac2c8057a
V <vnos@blackhole.consulting>
2022-12-31 18:20:57 +0000

    block base library

---
+++ block.py
@@ -0,0 +1,132 @@
+import json
+try:
+    import requests
+except:
+    pass
+
+
+nodes_api = ["http://192.168.1.139:9010"]
+
+## WARNING - Don't send unencrypted data
+
+def get_info():
+    """
+    /v1/chain/get_info
+    :return:
+    """
+
+    req = requests.post("block.blackhole.consulting",8888,"v1/chain/get_info","","") 
+        
+    return req
+
+def get_block(blocknum):
+    """
+    https://eosio.github.io/eos/group__eosiorpc.html#v1chaingetblock
+    :param block_num_or_id:
+    :return:
+    """
+
+    path = "/v1/chain/get_block"
+
+    req = requests.post("block.blackhole.consulting",8888,"v1/chain/get_block","\ncontent-type: application/json","{\"block_num_or_id\": \""+blocknum+"\"}")
+    
+    return req
+
+def get_account(account_name):
+    """
+    https://eosio.github.io/eos/group__eosiorpc.html#v1chaingetaccount
+    :param account_name:
+    :return:
+        """
+
+    path = "/v1/chain/get_account"
+
+    req = requests.post("block.blackhole.consulting",8888,"v1/chain/get_account","\ncontent-type: application/json","{\"account_name\": \""+account_name+"\"}")
+    
+    return req
+
+
+def get_code(account_name):
+    """
+    :param account_name:
+    :return:
+    """
+    path = "/v1/chain/get_code"
+
+    req = requests.post("block.blackhole.consulting",8888,"v1/chain/get_info","\ncontent-type: application/json","{\"account_name\": \""+account_name+"\"}")
+        
+    return req
+
+    
+
+def get_table_rows(scope: str, code: str, table: str, json: bool=True, lower_bound: int=None,
+               upper_bound: int=None, limit: int=None):
+	"""
+	https://eosio.github.io/eos/group__eosiorpc.html#v1chaingetcode
+	:param scope:
+	:param code:
+	:param table:
+	:param json:
+	:param lower_bound:
+	:param upper_bound:
+	:param limit:
+	:return:
+	"""
+
+	path = "/v1/chain/get_table_rows"
+
+
+	return requests.post(nodes_api[0]+path,9010,"\ncontent-type: application/json",'{"code": "string","table": "string","scope": "string","index_position": "string","key_type": "string","encode_type": "string","lower_bound": "string","upper_bound": "string","limit": 10,"reverse": false,"show_payer": false}')
+
+
+def push_transaction(ref_block_num: str, ref_block_prefix: str, expiration: str, scope: list,
+                 actions: list, signatures: list, authorizations: list):
+	"""
+	:param ref_block_num:
+	:param ref_block_prefix:
+	:param expiration: datetime  iosformat
+	:param scope:
+	:param actions:
+	:param signatures:
+	:param authorizations:
+	:return:
+	"""
+
+	path = "/v1/chain/push_transaction"
+
+
+	return requests.post(nodes_api[0]+path,9010,"\ncontent-type: application/json",'{"signatures": ["string"],"compression": true,"packed_context_free_data": "string","packed_trx": "string"}')
+
+
+
+def get_transaction(txid):
+
+	return 0 
+
+def get_acc_tx(acc):
+
+	return 0 
+
+def get_public_keys(self):
+	return 0
+
+
+def get_accounts():
+
+	return 0
+
+def genKeys():
+
+	return 0
+
+def getbalance(code,acc,symbol):
+	return 0
+
+def sign_transaction(self, transaction_list: list):
+	"""
+	:param transaction_list:
+	:return:
+	"""
+	return 0
+
+
---
+++ requests.py
@@ -0,0 +1,90 @@
+try:
+    import usocket as _socket
+except:
+    import _socket
+try:
+    import ussl as ssl
+except:
+    import ssl
+
+import socket
+
+def http(url,port):
+    _, _, host, path = url.split('/', 3)
+    addr = socket.getaddrinfo(host, port)[0][-1]
+    s = socket.socket()
+    s.settimeout(3.0)
+    s.connect(addr)
+    s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\n\r\n' % (path, host), 'utf8'))
+    while True:
+        data = s.recv(100)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+    s.close()
+
+
+def post(url,port,path,headers,dat):
+    addr = socket.getaddrinfo(url, port)[0][-1]
+    print("Address Info: ",addr)
+    s = socket.socket()
+    s.settimeout(3.0)
+    s.connect(addr)
+    s.send(bytes('POST /%s HTTP/1.0\r\nHost: %s%s\r\n\r\n%s' % (path, url, headers,dat), 'utf8'))
+    while True:
+        data = s.recv(100)
+        if data:
+            print(str(data, 'utf8'), end='')
+        else:
+            break
+    s.close()
+
+def https(url,port,method,dat,use_stream=True):
+    s = _socket.socket()
+
+    ai = _socket.getaddrinfo(url, port)
+    print("Address infos:", ai)
+    addr = ai[0][-1]
+
+    print("Connect address:", addr)
+    s.connect(addr)
+
+    s = ssl.wrap_socket(s)
+    print(s)
+
+    if use_stream:
+        # Both CPython and MicroPython SSLSocket objects support read() and
+        # write() methods.
+        s.write(b"GET / HTTP/1.0\r\n\r\n")
+        print(s.read(4096))
+    else:
+        # MicroPython SSLSocket objects implement only stream interface, not
+        # socket interface
+        s.send(b"GET / HTTP/1.0\r\n\r\n")
+        print(s.recv(4096))
+
+    s.close()
+
+def post_tls(url,port,path,method,headers,dat,use_stream=True):
+    s = _socket.socket()
+
+    ai = _socket.getaddrinfo(url, port)
+    print("Address infos:", ai)
+    addr = ai[0][-1]
+
+    print("Connect address:", addr)
+    s.connect(addr)
+
+    s = ssl.wrap_socket(s)
+    #print(s)
+
+    if use_stream:
+        # Both CPython and MicroPython SSLSocket objects support read() and
+        # write() methods.
+        s.write(b""+method+" /"+path+" HTTP/1.0\nHost:"+url+""+headers+"\r\n\r\n"+dat)
+        print(s.read(4096))
+
+    s.close()
+
+#main()
