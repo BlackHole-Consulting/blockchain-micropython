0c215e47108b9f4cc45684cbaa049334ef7d31ce
Hecatomb <60758685+pedroelbanquero@users.noreply.github.com>
2023-02-15 05:13:45 +0000

    Update block.py
    sign and send transactions in private blackhole blockchain EOS based

--- src/block.py
+++ src/block.py
@@ -5,6 +5,7 @@ except:
     pass
 
 
+import json
 
 from ufastecdsa import curve, ecdsa, keys, util
 
@@ -50,7 +51,7 @@ def get_block(blocknum):
 
     path = "/v1/chain/get_block"
 
-    req = requests.post("router.blackhole.consulting",8888,"v1/chain/get_block","Content-type: application/json","{\"block_num_or_id\": \""+str(blocknum)+"\"}")
+    req = requests.post("block.blackhole.consulting",8888,"v1/chain/get_block","Content-type: application/json","{\"block_num_or_id\": \""+str(blocknum)+"\"}")
     
     return req
 
@@ -68,6 +69,49 @@ def get_account(account_name):
     return req
 
 
+def get_balance(account_name,symbol):
+    """
+    https://eosio.github.io/eos/group__eosiorpc.html#v1chaingetaccount
+    :param account_name:
+    :return:
+        """
+
+    path = "v1/chain/get_currency_balance"
+
+    req = requests.post("block.blackhole.consulting",8888,path,"content-type: application/json","{\"code\":\"eosio.token\",\"account_name\": \""+account_name+"\",\"symbol\":\""+symbol+"\" }")
+    
+    return req
+
+def get_transaction(txid):
+    """
+    https://eosio.github.io/eos/group__eosiorpc.html#v1chaingetaccount
+    :param account_name:
+    :return:
+        """
+
+    path = "v1/history/get_transaction"
+
+    req = requests.post("block.blackhole.consulting",8888,path,"content-type: application/json","{\"blockNumHint\":null,\"id\": \""+txid+"\" }")
+
+    return req
+
+
+
+def get_actions(account_name,pos,offset):
+    """
+    https://eosio.github.io/eos/group__eosiorpc.html#v1chaingetaccount
+    :param account_name:
+    :return:
+        """
+
+    path = "v1/history/get_actions"
+
+    req = requests.post("block.blackhole.consulting",8888,path,"content-type: application/json","{\"pos\":"+pos+",\"account_name\": \""+account_name+"\",\"offset\":"+offset+" }")
+
+    return req
+
+
+
 def get_code(account_name):
     """
     :param account_name:
@@ -81,74 +125,33 @@ def get_code(account_name):
 
     
 
-def get_table_rows(scope: str, code: str, table: str, json: bool=True, lower_bound: int=None,
-               upper_bound: int=None, limit: int=None):
-	"""
-	https://eosio.github.io/eos/group__eosiorpc.html#v1chaingetcode
-	:param scope:
-	:param code:
-	:param table:
-	:param json:
-	:param lower_bound:
-	:param upper_bound:
-	:param limit:
-	:return:
-	"""
+def get_table_rows(code,table,scope):
 
-	path = "/v1/chain/get_table_rows"
+    """
+    https://eosio.github.io/eos/group__eosiorpc.html#v1chaingetaccount
+    :param account_name:
+    :return:
+        """
 
+    path = "/v1/chain/get_table_rows"
 
-	return requests.post(nodes_api[0]+path,9010,"\ncontent-type: application/json",'{"code": "string","table": "string","scope": "string","index_position": "string","key_type": "string","encode_type": "string","lower_bound": "string","upper_bound": "string","limit": 10,"reverse": false,"show_payer": false}')
-
-
-def push_transaction(ref_block_num: str, ref_block_prefix: str, expiration: str, scope: list,
-                 actions: list, signatures: list, authorizations: list):
-	"""
-	:param ref_block_num:
-	:param ref_block_prefix:
-	:param expiration: datetime  iosformat
-	:param scope:
-	:param actions:
-	:param signatures:
-	:param authorizations:
-	:return:
-	"""
-
-	path = "/v1/chain/push_transaction"
-
-
-	return requests.post(nodes_api[0]+path,9010,"\ncontent-type: application/json",'{"signatures": ["string"],"compression": true,"packed_context_free_data": "string","packed_trx": "string"}')
+    req = requests.post("block.blackhole.consulting",8888,"v1/chain/get_account","content-type: application/json","{\"code\":\""+code+"\",\"table\": \""+table+"\",\"scope\":\""+scope+"\"}")
+    
+    return req
 
 
 
-def get_transaction(txid):
+def send_transaction(fro, to,amount, symbol, memo, privkey):
 
-	return 0 
-
-def get_acc_tx(acc):
-
-	return 0 
-
-def get_public_keys(self):
-	return 0
+    ## TODO convert from EOS
+    path = "v1/chain/send_transaction"
+    trx = { "account": "eosio.token",  "name": "transfer",  "authorization": [{"actor": "eosio", "permission": "active"}], "data": {"from": fro,"to": to, "quantity": amount+" "+symbol,"memo": memo}}
 
 
-def get_accounts():
+    r,v = sign(trx,privkey)
+    
+    return requests.post("block.blackhole.consulting",8888,"\ncontent-type: application/json",'{"signatures": ["'+r+'","'+v+'"],"compression": true,"packed_context_free_data": "'+memo+'","packed_trx": '+json.dumps(trx)+'}')
 
-	return 0
 
-def genKeys():
-
-	return 0
-
-def getbalance(code,acc,symbol):
-	return 0
-
-def sign_transaction(self, transaction_list: list):
-	"""
-	:param transaction_list:
-	:return:
-	"""
-	return 0
 
 
